e073979972809cb29fc8a5e7fc7805f81d399beb
Aaron Trovillion <aaron.trovillion@gmail.com>
2020-06-03 12:23:01 -0400

    Add completed rcv-checkpoint work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,9 +5,10 @@ const base = 'http://localhost:3000';
 
 describe('routes : static', () => {
   describe('GET /', () => {
-    it('should return status code 200', done => {
+    it('should return status code 200 and have "Welcome to Bloccit 2020" in the body of the response', done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain('Welcome to Bloccit 2020');
         // Note that failure to use the done() method could result in false pass due to async
         done();
       });
