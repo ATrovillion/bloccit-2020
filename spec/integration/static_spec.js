10fb7e5f3c2791bb06c2b21a9affd11473ba3949
Aaron Trovillion <aaron.trovillion@gmail.com>
2020-06-03 17:58:05 -0400

    Add completed rcv-submission work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,6 +2,7 @@ const request = require('request');
 const server = require('../../src/server');
 
 const base = 'http://localhost:3000';
+const about = 'http://localhost:3000/about';
 
 describe('routes : static', () => {
   describe('GET /', () => {
@@ -10,6 +11,16 @@ describe('routes : static', () => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain('Welcome to Bloccit 2020');
         // Note that failure to use the done() method could result in false pass due to async
+        done();
+      });
+    });
+  });
+
+  describe('GET /about', () => {
+    it('should return status code 200 and have "About Bloccit 2020" in the body of the response', done => {
+      request.get(about, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain('About Bloccit 2020');
         done();
       });
     });
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -2,4 +2,8 @@ module.exports = {
   index(req, res, next) {
     res.render('static/index', { title: 'Welcome to Bloccit 2020' });
   },
+
+  about(req, res, next) {
+    res.render('static/about', { title: 'About Bloccit 2020' });
+  },
 };
