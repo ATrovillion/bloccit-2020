cba21c6b1c904d1b594ec146e1f6a9a32b76403b
Aaron Trovillion <aaron.trovillion@gmail.com>
2020-06-03 09:38:01 -0400

    Add completed express-intro-assignment work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,6 +2,7 @@ const request = require('request');
 const server = require('../../src/server');
 
 const base = 'http://localhost:3000';
+const marco = 'http://localhost:3000/marco';
 
 describe('routes : static', () => {
   describe('GET /', () => {
@@ -9,6 +10,16 @@ describe('routes : static', () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
+        done();
+      });
+    });
+  });
+
+  describe('GET /marco', () => {
+    it('should return status code 200', done => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe('polo!');
         done();
       });
     });
