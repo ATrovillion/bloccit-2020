8d087134a062803b9b3e4fedf20c08fca7aa9bb2
Aaron Trovillion <aaron.trovillion@gmail.com>
2020-06-03 10:36:23 -0400

    Add completed deploying-production-checkpoint code prior to heroku push

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,7 +8,7 @@ describe('routes : static', () => {
     it('should return status code 200', done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
+        // Note that failure to use the done() method could result in false pass due to async
         done();
       });
     });
